a22b4f261adc75efb103cd0427edc038907594c8
HidayatRf <rezafairuzh54@gmail.com>
2022-12-01 10:00:12 +0700

    menyelesaikan respinsive [ 576px - 1400px ]

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
 	navlist.style.top = navbar.clientHeight + "px"
-	
+
 })
 
 const navbar = document.querySelector('.navbar')
@@ -25,28 +25,31 @@ clNav.addEventListener('click', () => {
 })
 
 window.addEventListener("scroll", () => {
+
+	navbar.style.backgroundColor = "rgba(4, 92, 144, 1)"
+
+	if (window.innerWidth > 850) {
+		navlist.style.backgroundColor = "transparent"
+	}
+
 	navbar.style.boxShadow = "0 0 20px rgba(40, 150, 220, .6)";
-	
-	if ( Math.floor(window.scrollY) < 2 && !opened ) {
-		navbar.style.boxShadow = "0 0 20px rgba(40, 80, 180, 0)";
+	if (window.innerWidth <= 600) {
+		if (Math.floor(window.scrollY) < 2 && !opened) {
+			navbar.style.boxShadow = "0 0 20px rgba(40, 80, 180, 0)";
+			navbar.style.backgroundColor = "rgba(4, 92, 144, 0)"
+		}
+
+		if (opened || Math.floor(window.scrollY) < 2 || Math.floor(window.scrollY) > 0) {
+			navlist.style.backgroundColor = "rgba(18, 120, 176, .7)"
+			navbar.style.backgroundColor = "rgba(4, 92, 144, 1)"
+			navbar.style.boxShadow = "0 0 20px rgba(40, 80, 180, 0)";
+		}
+	}
+
+	if (!opened && Math.floor(window.scrollY) < 3) {
+		navlist.style.backgroundColor = "rgba(0, 0, 0, 0)"
 		navbar.style.backgroundColor = "rgba(4, 92, 144, 0)"
-	}
-	
-	if ( opened || Math.floor(window.scrollY) < 2 || Math.floor(window.scrollY) > 0) {
-		navlist.style.backgroundColor = "rgba(18, 120, 176, .7)"
-		navbar.style.backgroundColor = "rgba(4, 92, 144, 1)"
 		navbar.style.boxShadow = "0 0 20px rgba(40, 80, 180, 0)";
 	}
-	
-	if ( !opened && Math.floor(window.scrollY) < 3){
-		navlist.style.backgroundColor = "rgba(0, 0, 0, .2)"
-		navbar.style.backgroundColor = "rgba(4, 92, 144, 0)"
-		navbar.style.boxShadow = "0 0 20px rgba(40, 80, 180, 0)";
-	}
-	
-	
+
 })
-
-
-
-
